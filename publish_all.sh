c7cba0e4e904087627f71c04feddca34e1c9631f
Sriram Sami <frizensami@gmail.com>
2020-01-01 16:12:26 +0800

    Fix cd bug in publish_all script

--- publish_all.sh
+++ publish_all.sh
@@ -1,1 +1,8 @@
-cd plagiarismbasic_lib/ && cargo publish && echo "Sleeping to allow cargo publish for lib to take effect" && sleep 10 & cd ../plagiarism-basic && cargo publish && cd ..
+#!/bin/bash
+cd plagiarismbasic_lib/;
+cargo publish;
+echo "Sleeping to allow cargo publish for lib to take effect";
+sleep 10; 
+cd ../plagiarism-basic;
+cargo publish;
+cd ..;
