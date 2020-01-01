695832be124a739281d6ae6dda554a2ebbb77e58
Sriram Sami <frizensami@gmail.com>
2020-01-01 13:42:20 +0800

    Upgrade all scripts to use release build

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cargo run -- -t plagiarismbasic_lib/testfiles/t/ -u plagiarismbasic_lib/testfiles/ut/ -m equal -n 5 -s 0 --openhtml --cli --html
+cargo run --release -- -t plagiarismbasic_lib/testfiles/t/ -u plagiarismbasic_lib/testfiles/ut/ -m equal -n 5 -s 0 --openhtml --cli --html
